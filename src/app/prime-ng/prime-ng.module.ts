0b47f024e25684f3443c9fe0e0fbcdd61f36e04c
GeovannyTS <yowtecum@gmail.com>
2023-08-22 18:23:46 -0600

    implementacion de navbar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MenuModule } from 'primeng/menu';
+import { MenuModule  } from 'primeng/menu';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { MenubarModule } from 'primeng/menubar';
 
 
 
 @NgModule({
   exports:[
-    MenuModule,
+    MenubarModule,
   ],
 })
 export class PrimeNgModule { }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,33 +1,65 @@
-import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { isNgContainer } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { MegaMenuItem, MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {
-
+export class MenuComponent implements OnInit {
   public menuItems: MenuItem[] = [];
 
-    ngOnInit() {
-        this.menuItems = [
-            {
-                label: 'pipes de Angular',
-                icon: 'pi pi-fw pi-plus',
-            },
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash'
-            },
-            {
-              label: 'open',
-              icon: 'pi pi-fw pi-download',
-            },
-            {
-            label: 'undo',
-            icon: 'pi pi-fw pi-refresh',
-            },
-        ];
-    }
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-fw pi-desktop',
+        items: [
+          {
+            label: 'textos y fechas',
+            icon: 'pi pi-fw pi-align-left',
+          },
+          {
+            label: 'numeros',
+            icon: 'pi pi-fw pi-dollar',
+          },
+
+          {
+            label: 'No comunes',
+            icon: 'pi pi-fw pi-globe',
+          },
+          {
+            separator: true,
+          },
+        ],
+      },
+      {
+        label: 'Pipes personales',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {
+            label: 'Elemento 1',
+            icon: 'pi pi-fw pi-align-left',
+          },
+          {
+            label: 'Elemento 2',
+            icon: 'pi pi-fw pi-align-right',
+          },
+          {
+            label: 'Elemento 3',
+            icon: 'pi pi-fw pi-align-center',
+          },
+          {
+            label: 'Elemento 4',
+            icon: 'pi pi-fw pi-align-justify',
+          },
+        ],
+      },
+      {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off',
+      },
+    ];
+  }
 }
